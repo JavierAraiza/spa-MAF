3a99367f49b887e18fd9de75f1c676bf3bdc65ed
JavierAraiza <99153036+JavierAraiza@users.noreply.github.com>
2022-11-10 19:11:09 -0600

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,35 @@
+//Menu Open Close 
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+
+};
+
+//Swiper
+var swiper = new Swiper(".services-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 30000,
+      disableOnInteraction: false,
+    },
+  /*  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }, */
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  //Header
+  let header = document.querySelector("header");
+
+  window.addEventListener("scroll", () => { 
+    header.classList.toggle("shadow", window.scrollY > 0);
+  });
+
+  
